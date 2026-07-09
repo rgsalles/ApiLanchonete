3aee200311d8686a17806230f9606ad72e731ce2
rgsalles <rodrigo.glsalles@gmail.com>
2026-07-09 01:28:27 -0300

    Iniciando DbContext

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,14 @@
+using ApiLanchonete.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ApiLanchonete.Data;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options)
+        : base(options)
+    {
+    }
+
+    public DbSet<Product> Products { get; set; }
+}
