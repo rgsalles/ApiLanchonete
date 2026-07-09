5b67c904ebbffcce53c7854732d0c9b8bfa8f673
rgsalles <rodrigo.glsalles@gmail.com>
2026-07-09 01:23:00 -0300

    Initializing project

---
+++ Models/Product.cs
@@ -0,0 +1,13 @@
+namespace ApiLanchonete.Models;
+
+public class Product
+{
+    public Guid ProductId { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public decimal Price { get; set; }
+    public string Description { get; set; } = string.Empty;
+    public string Image { get; set; } = string.Empty;
+    public bool Active { get; set; }
+    public DateTime? AvailableFrom { get; set; }
+    public DateTime? AvailableUntil { get; set; }
+}
