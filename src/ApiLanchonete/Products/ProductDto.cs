f9032db581c57bb321d3a28f93239ca4667754d7
rgsalles <rodrigo.glsalles@gmail.com>
2026-08-01 13:15:35 -0300

    Simplifying Dtos

--- src/ApiLanchonete/Products/ProductDto.cs
+++ src/ApiLanchonete/Products/ProductDto.cs
@@ -11,3 +11,24 @@ public class ProductDto
     public DateTime? AvailableFrom { get; set; }
     public DateTime? AvailableUntil { get; set; }
 }
+
+public class CreateProductDto
+{
+    public string Name { get; set; } = string.Empty;
+    public decimal Price { get; set; }
+    public string Description { get; set; } = string.Empty;
+    public string Image { get; set; } = string.Empty;
+    public DateTime? AvailableFrom { get; set; }
+    public DateTime? AvailableUntil { get; set; }
+}
+
+public class UpdateProductDto
+{
+    public string Name { get; set; } = string.Empty;
+    public decimal Price { get; set; }
+    public bool Active { get; set; }
+    public string Description { get; set; } = string.Empty;
+    public string Image { get; set; } = string.Empty;
+    public DateTime? AvailableFrom { get; set; }
+    public DateTime? AvailableUntil { get; set; }
+}
