2d26c825b3b8f187861e911b7a26f3f287580c2c
rgsalles <rodrigo.glsalles@gmail.com>
2026-08-02 17:56:43 -0300

    Creating companies and branches

---
+++ src/ApiLanchonete/Features/Branches/Branch.cs
@@ -0,0 +1,7 @@
+﻿public class Branch
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public Guid CompanyId { get; set; }
+    public Company Company { get; set; } = null!;
+}
---
+++ src/ApiLanchonete/Features/Companies/Company.cs
@@ -0,0 +1,10 @@
+﻿public class Company
+{
+    public Guid Id { get; set; }
+
+    public string Name { get; set; } = string.Empty;
+
+    public string Cnpj { get; set; } = string.Empty;
+
+    public ICollection<Branch> Branches { get; set; } = [];
+}
